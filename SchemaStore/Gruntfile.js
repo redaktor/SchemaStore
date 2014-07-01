38032025f69135d7d3e61cca0ff29d5d1e52d2b8
Mads Kristensen <post@madskristensen.net>
2014-06-30 17:20:32 -0700

    Added grunt and Schema validation tests

---
+++ SchemaStore/Gruntfile.js
@@ -0,0 +1,29 @@
+﻿/*global module */
+module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        tv4: {
+            catalog: {
+                options: {
+                    root: grunt.file.readJSON("schemas/json/schema-catalog.json")
+                },
+                src: ["api/json/catalog.json"]
+            },
+
+            schemas: {
+                options: {
+                    fresh: true,
+                    root: grunt.file.readJSON("test/hyper-schema.json"),
+                },
+                src: ["schemas/json/*.json"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-tv4");
+
+    grunt.registerTask("default", ["tv4"]);
+};
