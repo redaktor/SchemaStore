dc65b2a2ecc817e702919e0d6894be83fd7c17c5
Mads Kristensen <post@madskristensen.net>
2014-07-19 13:20:01 -0700

    Refactored gruntfile

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -18,42 +18,44 @@ module.exports = function (grunt) {
                     root: grunt.file.readJSON("test/hyper-schema.json"),
                 },
                 src: ["schemas/json/*.json"]
-            }
-        }
-    });
-
-    // Dynamically load schema validation based on the files and folders in /test/ 
-    var fs = require("fs");
-    var dir = "test";
-    var folders = fs.readdirSync(dir);
-
-    folders.forEach(function (folder) {
-
-        // If it's a file, ignore and continue. We only care about folders.
-        if (folder.indexOf('.') > -1)
-            return;
-
-        var schema = grunt.file.readJSON("schemas/json/" + folder.replace("_", ".") + ".json");
-        var files = fs.readdirSync(dir + "/" + folder).map(function (file) { return dir + "/" + folder + "/" + file });
-
-        grunt.config.set("tv4." + folder, {
+            },
             options: {
-                root: schema,
                 schemas: {
                     "http://json-schema.org/draft-04/schema#": grunt.file.readJSON("test/hyper-schema.json"),
                     "http://schemastore.org/schemas/json/jshintrc.json": grunt.file.readJSON("schemas/json/jshintrc.json"),
                     "http://schemastore.org/schemas/json/grunt-task.json": grunt.file.readJSON("schemas/json/grunt-task.json")
                 },
-            },
+            }
+        }
+    });
 
-            src: files
+    grunt.registerTask("setup", "Dynamically load schema validation based on the files and folders in /test/", function () {
+        var fs = require("fs");
+        var dir = "test";
+        var folders = fs.readdirSync(dir);
+
+        folders.forEach(function (folder) {
+
+            // If it's a file, ignore and continue. We only care about folders.
+            if (folder.indexOf('.') > -1)
+                return;
+
+            var schema = grunt.file.readJSON("schemas/json/" + folder.replace("_", ".") + ".json");
+            var files = fs.readdirSync(dir + "/" + folder).map(function (file) { return dir + "/" + folder + "/" + file });
+
+            grunt.config.set("tv4." + folder, {
+                options: {
+                    root: schema
+                },
+                src: files
+            });
+
+            // Write the config to disk so it can be consumed by the browser based test infrastru
+            fs.writeFile("test/tests.json", JSON.stringify(grunt.config.get("tv4")));
         });
-
-        // Write the config to disk so it can be consumed by the browser based test infrastru
-        fs.writeFile("test/tests.json", JSON.stringify(grunt.config.get("tv4")));
     });
 
     grunt.loadNpmTasks("grunt-tv4");
 
-    grunt.registerTask("default", ["tv4"]);
+    grunt.registerTask("default", ["setup", "tv4"]);
 };
